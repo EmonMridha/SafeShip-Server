ae3a7e9117f8febfd87733ece7d2aeeeeb41813d
EmonMridha <emonmridha712@gmail.com>
2026-01-28 22:12:16 +0600

    added payment system

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // Load environment variables from .env file
 dotenv.config();
 
+const stripe = require('stripe')(process.env.PAYMENT_GATEWAY_KEY);
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -34,14 +36,15 @@ async function run() {
 
         const db = client.db('SafeShip')
         const parcelCollection = db.collection('parcels');
+        const paymentCollection = db.collection('payments');
 
         app.post('/parcels', async (req, res) => {
             try {
-                const parcelData = req.body;
+                const parcelData = req.body; // getting parcel data from the client
                 parcelData.createdAt = new Date();
 
-                const result = await parcelCollection.insertOne(parcelData)
-                res.send(result);
+                const result = await parcelCollection.insertOne(parcelData) // inserting parcel data in the database and saving the confirmation message here
+                res.send(result); // sending the confirmation message to the client
             }
             catch (error) {
                 res.status(500).send({ message: 'Failed to create' })
@@ -50,13 +53,13 @@ async function run() {
 
         // Get parcels by email
         app.get('/parcels/:email', async (req, res) => {
-            const email = req.params.email;
-            const query = { userEmail: email };
+            const email = req.params.email; // getting email from the url
+            const query = { userEmail: email }; // creating query to find parcels by email. Query is like roll number of a student
             const options = {
                 sort: { createdAt: -1 }
             }
-            const result = await parcelCollection.find(query, options).toArray()
-            res.send(result)
+            const result = await parcelCollection.find(query, options).toArray() // commanding the db to find data matching with the query and save here
+            res.send(result) // sending the data to the client
         })
 
         // Get parcel by id
@@ -73,6 +76,57 @@ async function run() {
             const query = { _id: new ObjectId(id) } // Converging into mongodb id
             const result = await parcelCollection.deleteOne(query) // Commanding to delete the data matching with the query and saving the confirmation message here
             res.send(result) // sending the confirmation message to the client
+        })
+
+
+        app.post('/payments', async (req, res) => {
+            try {
+                const { parcelId, email, amount, paymentMethod, transactionId } = req.body; // getting payment data from the client
+
+                // Update parcel payment status
+                const updateResult = await parcelCollection.updateOne(
+                    { _id: new ObjectId(parcelId) },
+                    { $set: { paymentStatus: 'paid' } }
+                ); // updating the doc matching with the parcelId 
+
+                if (updateResult.modifiedCount === 0) {
+                    return res.status(404).send({ message: 'Already paid' }); 
+                } 
+
+                // insert payment record
+                const paymentDoc = {
+                    parcelId,
+                    email,
+                    amount,
+                    paymentMethod,
+                    transactionId,
+                    createdAt: new Date()
+                };
+
+                const result = await paymentCollection.insertOne(paymentDoc); // inserting payment record in the database and saving the confirmation message here
+                res.status(201).send({
+                    success: true,
+                    message: 'Payment recorded successfully',
+                }); // Sending the confirmation message to the client
+            }
+            catch (error) {
+                res.status(500).send({ message: 'Payment processing failed' });
+            }
+        })
+
+        app.post('/create-payment-intent', async (req, res) => {
+            const amountInCents = req.body.amount // getting amount from the client
+            try {
+                const paymentIntent = await stripe.paymentIntents.create({
+                    amount: amountInCents, // amount in cents
+                    currency: 'usd',
+                    payment_method_types: ['card']
+                })
+                res.json({ clientSecret: paymentIntent.client_secret })
+            }
+            catch (error) {
+                res.status(500).json({ error: error.message });
+            }
         })
 
 
