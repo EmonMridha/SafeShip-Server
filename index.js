632cbfab594b98cf4b226889f071910177600a96
EmonMridha <emonmridha712@gmail.com>
2026-01-30 14:02:58 +0600

    added thing

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v9x5iie.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
